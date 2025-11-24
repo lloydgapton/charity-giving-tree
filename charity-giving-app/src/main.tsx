64f264c9d523b49831dee7808380613ee814c519
Lloydgapton <lloydgapton123@gmail.com>
2025-11-24 03:35:42 -0800

    used HashRouter

--- charity-giving-app/src/main.tsx
+++ charity-giving-app/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'    
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <App /> 
+    </HashRouter>
   </StrictMode>
 );
