1ee915da88f4c3d00c7f6758b3531ca5a2ff482a
Lloydgapton <lloydgapton123@gmail.com>
2025-11-24 03:49:15 -0800

    fixing routing

--- charity-giving-app/src/App.tsx
+++ charity-giving-app/src/App.tsx
@@ -1,14 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/Landing';
 import CharityPage from './pages/Charity/[slug]/Charity';
 
 function App() {
   return (
-    <Routes>
+    <>
+        <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/charity/:slug" element={<CharityPage />} />
-    </Routes>
+
+        </Routes>
+    
+        <Link to="#/charity/evergreen-foundation">Visit Charity</Link>
+
+    </>
   );
+
 }
 
 export default App;
